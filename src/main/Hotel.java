da5b41b6850a2d5d99f0550c6d96bdb9e720d99e
Dua-balouch <dua.67718@iqra.edu.pk>
2026-01-18 17:46:43 +0500

    Develop core business logic for Hotel and Reservation management

---
+++ src/main/Hotel.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Hotel {
+    private String hotelName;
+
+    public Hotel(String name) {
+        this.hotelName = name;
+    }
+
+    public String getHotelName() { return hotelName; }
+}
+    
+
---
+++ src/main/Reservation.java
@@ -0,0 +1,4 @@
+package main;
+public class Reservation {
+    public String getConfirmation() { return "Confirmed"; }
+}
